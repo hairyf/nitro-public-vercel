836f09dbed026b8cb4b0609fbd83ba53b9ab5e52
hairyf <wwu710632@gmail.com>
2025-07-03 23:35:09 +0800

    Add initial HTML template for Vite + React + TypeScript setup

---
+++ server/public/assets/_...all_-BRo9q88g.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-CY5346Bx.js";function n(){return o.jsx("div",{children:"Not Found"})}export{n as default};
---
+++ server/public/assets/_name_-DLIGdlZD.js
@@ -0,0 +1,1 @@
+import{u as e,j as o}from"./index-CY5346Bx.js";function n(){console.log($fetch);const{name:s}=e();return o.jsxs("div",{children:["Hello ",s]})}export{n as default};
